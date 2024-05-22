0741fb3400650e9e0a48417b99d054e8f8a2130e
Ruslan-Mazhuga1982 <167924377+Ruslan-Mazhuga1982@users.noreply.github.com>
2024-05-22 23:35:55 +0300

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,9 @@
+from module2 import hi_Word as hw, hi_Word_2 as hw2
+
+
+from module2 import *
+hw(), hw2()
+
+print(dir(hw2()), dir(hw()))
+
+
---
+++ module2.py
@@ -0,0 +1,18 @@
+def hi_Word():
+    print('Привет я из функции во втором модуле')
+    print('Hello Word')
+
+def hi_Word_2():
+    print('Привет я все еще из функции во втором модуле')
+    print('Hello Word')
+
+
+
+# def main():
+#     a = 5
+#     b = 10
+#     print('Привет')
+#
+#
+# if __name__ == '__main__':
+#     main()
